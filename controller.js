35a5a3c55edf24410468c3cafeea8a210363855b
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-10-25 13:32:37 -0600

    quotes are working

--- controller.js
+++ controller.js
@@ -1,3 +1,10 @@
 angular.module('quoteBook').controller('mainCtrl', function($scope, service){
-  $scope.test = "Hello World!"
+  $scope.test = "Hello World!";
+
+  $scope.getQuotes = function(){
+    $scope.quotes = service.getData();
+  };
+
+  $scope.getQuotes();
+
 });
